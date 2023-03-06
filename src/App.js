2708c74d4a5be7a379abdecb0abd5882c7122731
Ross Condie <rosscondie@gmail.com>
2023-03-06 18:25:59 +0100

    items successfully added to list

--- src/App.js
+++ src/App.js
@@ -2,8 +2,47 @@ import './App.css';
 import React, {useState} from 'react';
 
 function App() {
+  const [todos, setTodos] = useState([
+    { name: "Buy shopping", priority: "high" }, 
+    { name: "Clean bathroom", priority: "low" }, 
+    { name: "Car's MOT", priority: "high" }, 
+  ]);
+  const [newItemName, setNewItemName] = useState("");
+
+  function saveNewItem(event) {
+    event.preventDefault();
+    const newItem = {
+      name: newItemName,
+      priority: "low",
+    }
+    const newItems = [...todos, newItem];
+    setTodos(newItems);
+    setNewItemName("");
+
+  }
+
+  function handleInputChange(event) {
+    setNewItemName(event.target.value)
+  }
+
+  const todoNodes = todos.map((todo, index) => {
+    return <li key={index}>
+      <span>{todo.name}</span>
+    </li>
+  });
+
   return (
-    <h1>Hello World</h1>
+    <div className="App">
+      <h1>ToDo's</h1>
+      <form onSubmit={saveNewItem}>
+        <label></label>
+        <input type="text" value={newItemName} onChange={handleInputChange}/>
+        <input type="submit" value="Save Item" />
+      </form>
+      <ul>
+       {todoNodes}
+      </ul>
+    </div>
   );
 }
 
